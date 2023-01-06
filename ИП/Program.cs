751617e5b2237e5ee7bde9142e5abe19ae998c64
AlisaKozlovskaya <116607568+AlisaKozlovskaya@users.noreply.github.com>
2023-01-07 01:04:46 +0300

    Add files via upload

---
+++ ИП/Program.cs
@@ -0,0 +1,42 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма.
+
+Console.WriteLine("Введите число элементов массива:");
+int N = int.Parse(Console.ReadLine());
+
+if (N <= 0) //проверка на отрицательность размера массива
+{
+    Console.WriteLine("Введите число больше нуля");
+}
+
+void PrintArray(string[] arr) // метод печати массива
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] array = new string[N]; 
+for (int i = 0; i < N; i++) // заполнение исходного массива
+    {
+        Console.WriteLine($"Введите {i+1}-й элемент массива из {N}:");
+        array[i] = Console.ReadLine();
+        Console.WriteLine();
+    }
+PrintArray(array);
+
+int count = 0;
+string[] arrayNoMoreThree = new string[array.Length];
+for (int i = 0; i < N; i++)
+    {
+    if (array[i].Length <= 3)
+        {
+        arrayNoMoreThree[count] = array[i];
+        count++;
+        }
+    }
+
+PrintArray(arrayNoMoreThree);
